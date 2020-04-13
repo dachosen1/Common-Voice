f3720e18b7e453f9bc15622eedfb79ccc97e58a3
dachosen1 <anderson.nelson1@gmail.com>
2020-04-12 23:12:20 -0700

    file caused this error:
    TypeError: join() argument must be str or bytes, not 'PosixPath'

--- model/__init__.py
+++ model/__init__.py
@@ -1,9 +1,9 @@
-import os
-import pathlib
-
-import model
-
-PACKAGE_ROOT = pathlib.Path(model.__file__).resolve().parent
-
-with open(os.path.join(PACKAGE_ROOT, "VERSION")) as version_file:
-    __version__ = version_file.read().strip()
+# import os
+# import pathlib
+#
+# import model
+#
+# PACKAGE_ROOT = pathlib.Path(model.__file__).resolve().parent
+#
+# with open(os.path.join(PACKAGE_ROOT, "VERSION")) as version_file:
+#     __version__ = version_file.read().strip()
