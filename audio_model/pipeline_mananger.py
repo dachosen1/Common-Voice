495087f62b62363cbd0aed55b0625e62b9f2dfa7
Anderson Nelson <anderson.nelson1@gmail.com>
2020-08-21 13:05:21 -0700

    Revert "make it better"
    This reverts commit 9cf6eab2

---
+++ audio_model/pipeline_mananger.py
@@ -0,0 +1,59 @@
+import logging
+import os
+import typing as t
+
+import joblib
+
+from audio_model import LSTM
+from audio_model import __version__ as _version
+from audio_model.config.config import TRAINED_MODEL_DIR
+
+_logger = logging.getLogger(__name__)
+
+
+def remove_old_pipelines(*, files_to_keep: t.List[str]) -> None:
+    """
+    Remove old model pipelines. This is to ensure there is a simple one-to-one mapping between the package version
+    and the model version to be imported and used by other applications. However, we do also include the immediate
+    previous pipeline version for differential testing purposes.
+    """
+    do_not_delete = files_to_keep + ["__init__.py"]
+    for model_file in TRAINED_MODEL_DIR.iterdir():
+        if model_file.name not in do_not_delete:
+            model_file.unlink()
+
+
+def save_pipeline(*, pipeline_to_persist, model_name) -> None:
+    """
+    Saves the versioned model, and overwrites any previous saved models. This ensures that when the package is
+    published, there is only one trained model that can be called, and we know exactly how it was built.
+    :param pipeline_to_persist:
+    :return:
+    """
+
+    save_file_name = "{}{}.pth".format(model_name.MODEL_NAME, _version)
+    save_path = TRAINED_MODEL_DIR / save_file_name
+
+    remove_old_pipelines(files_to_keep=[save_file_name])
+    joblib.dump(pipeline_to_persist, save_path)
+    _logger.info("saved pipeline: {}".format(save_file_name))
+
+
+def load_model(model_name) -> object:
+    """
+    Load a saved PyTorch model
+    :param model_name:  Name of the model to parse
+    :return: torch model and model path
+    """
+
+    model = LSTM.AudioLSTM(
+        num_layer=model_name.PARAM['NUM_LAYERS'],
+        hidden_size=model_name.PARAM['HIDDEN_DIM'],
+        input_size=model_name.PARAM['INPUT_SIZE'],
+        output_size=model_name.PARAM['OUTPUT_SIZE'],
+        dropout=model_name.PARAM['DROPOUT'],
+        batch_size=1
+    )
+    model.eval()
+    file_path = os.path.join(TRAINED_MODEL_DIR, model_name.NAME + _version + ".pt")
+    return model, file_path
---
+++ audio_model/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+# Package meta-data.
+NAME = "audio_model"
+
+DESCRIPTION = "Approach to classify person age, gender country of origin based on voice"
+
+long_description = (
+    "This modules uses data from Common voice which is a crowdsourcing project started "
+    "by Mozilla to create a free database for speech recognition software. The project is supported by "
+    "volunteers who record sample sentences with a microphone and review recordings of other users."
+    " The transcribed sentences will be collected in a voice database available under the public domain "
+    "license CC0. This license ensures that developers can use the database for voice-to-text "
+    "applications without restrictions or costs. Common Voice appeared as a response to the language "
+    "assistants of large companies such as Amazon Echo, Siri or Google Assistant. "
+    ""
+    ""
+    "In this modules I will provide an approach to classify in real time a person voice into the three "
+    "broad categories of the commonvoice-voice-voice data set"
+)
+
+EMAIL = "anderson.nelson1@gmail.com"
+AUTHOR = "Anderson Nelson"
+REQUIRES_PYTHON = '>=3.5.4'
+
+URL = 'https://github.com/dachosen1/Common-Voice'
+
+
+# What packages are required for this module to be executed?
+def list_reqs(name="requirements.txt"):
+    with open(name) as fd:
+        return fd.read().splitlines()
+
+
+# Load the package's __version__.py module as a dictionary.
+
+PACKAGE_ROOT = os.path.join(os.getcwd())
+about = {}
+with open(os.path.join(PACKAGE_ROOT, 'VERSION.txt')) as f:
+    _version = f.read().strip()
+    about['__version__'] = _version
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=('tests',)),
+    package_data={'audio_model': ['__version__']},
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    install_requires=list_reqs(),
+    include_package_data=True,
+    license="MIT",
+)
