ada276d3e8e0a336cae95017ede0ed8c04d69e79
dachosen1 <anderson.nelson1@gmail.com>
2020-09-07 13:25:54 -0700

    Package re-organization

--- run.sh
+++ run.sh
@@ -1,14 +1,2 @@
-#!/usr/bin/env bash
 export IS_DEBUG=${DEBUG:-false}
-## shellcheck disable=SC2155
-#export UID=$(id -u)
-## shellcheck disable=SC2155
-#export GID=$(id -g)
-#docker build --build-arg USER="$USER" \
-#             --build-arg UID="$UID" \
-#             --build-arg GID="$GID" \
-#             --build-arg PW=<PASSWORD IN CONTAINER> \
-#             -t common-voice-app \
-#             -f common-voice \
-#             .
 exec gunicorn --bind "0.0.0.0:${PORT:-5000}" --access-logfile - --error-logfile - run_app:app
