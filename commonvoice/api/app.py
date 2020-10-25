dbf94bbbb7897ec257d9331774d0d82c035d446a
dachosen1 <anderson.nelson1@gmail.com>
2020-10-24 23:50:08 -0700

    test model deployment

--- commonvoice/api/app.py
+++ commonvoice/api/app.py
@@ -19,28 +19,27 @@ app = Flask(__name__, static_folder="css", static_url_path="/css",
 
 socketio = SocketIO(app)
 
-# FORMAT = pyaudio.paFloat32
-# CHANNELS = 1
-
+FORMAT = pyaudio.paFloat32
+CHANNELS = 1
 
 # Gender Model
-# model_gender, path_gender = load_model(model_name=CommonVoiceModels.Gender)
-# model_gender.load_state_dict(torch.load(path_gender, map_location=torch.device('cpu')))
-# model_gender.eval()
-# model_gender.init_hidden()
-#
-# if torch.cuda.is_available():
-#     model_gender.cuda()
-#
-# max_frames = 50
-# frames = []
+model_gender, path_gender = load_model(model_name=CommonVoiceModels.Gender)
+model_gender.load_state_dict(torch.load(path_gender, map_location=torch.device('cpu')))
+model_gender.eval()
+model_gender.init_hidden()
+
+if torch.cuda.is_available():
+    model_gender.cuda()
+
+max_frames = 50
+frames = []
 
 
-# def callback(in_data, frame_count, time_info, status):
-#     frames.append(np.frombuffer(in_data, dtype=np.int16))
-#     if len(frames) > max_frames:
-#         frames.pop(0)
-#     return in_data, pyaudio.paContinue
+def callback(in_data, frame_count, time_info, status):
+    frames.append(np.frombuffer(in_data, dtype=np.int16))
+    if len(frames) > max_frames:
+        frames.pop(0)
+    return in_data, pyaudio.paContinue
 
 
 @app.route("/")
@@ -51,30 +50,29 @@ def index():
 
 @socketio.on('audio-streaming', )
 def run_audio_stream(msg):
-    # p = pyaudio.PyAudio()
-    # stream = p.open(
-    #     format=FORMAT,
-    #     channels=CHANNELS,
-    #     rate=CommonVoiceModels.Frame.FRAME["SAMPLE_RATE"],
-    #     input=True,
-    #     stream_callback=callback,
-    # )
-    # stream.start_stream()
+    p = pyaudio.PyAudio()
+    stream = p.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=CommonVoiceModels.Frame.FRAME["SAMPLE_RATE"],
+        input=True,
+        stream_callback=callback,
+    )
+    stream.start_stream()
     while True:
+        if len(frames) >= 32:
+            socketio.sleep(0.5)
 
-        socketio.sleep(0.5)
+            signal = np.concatenate(tuple(frames))
+            wave_period = signal[-CommonVoiceModels.Frame.FRAME["SAMPLE_RATE"]:].astype(np.float)
+            spectrogram = audio_melspectrogram(wave_period)
 
-        # signal = np.concatenate(tuple(frames))
-        # wave_period = signal[-CommonVoiceModels.Frame.FRAME["SAMPLE_RATE"]:].astype(np.float)
-        # spectrogram = audio_melspectrogram(wave_period)
-
-        # Gender Model
-        # gender_output, gender_prob = generate_pred(mel=spectrogram, model=model_gender,
-        #                                            label=CommonVoiceModels.Gender.OUTPUT,
-        #                                            model_name=CommonVoiceModels.Gender,
-        #                                            )
-
-        socketio.emit('gender_model', {'pred': 'gender_output', 'prob': round(np.random.randint(0, 1000) / 100, 2)})
+            # Gender Model
+            gender_output, gender_prob = generate_pred(mel=spectrogram, model=model_gender,
+                                                       label=CommonVoiceModels.Gender.OUTPUT,
+                                                       model_name=CommonVoiceModels.Gender,
+                                                       )
+            socketio.emit('gender_model', {'pred': gender_output, 'prob': round(np.random.randint(0, 100))})
 
 
 @app.route("/about/")
