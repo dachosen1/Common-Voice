dfa864c0bba771255475c2a109bc41977d99b37a
Anderson Nelson <anderson.nelson1@gmail.com>
2020-12-17 20:25:37 -0800

    Remove absoloute path

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export IS_DEBUG=${DEBUG:-false}
-exec gunicorn --bind "0.0.0.0:${PORT:-8080}" -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 module::app
+exec gunicorn --bind "0.0.0.0:${PORT:-8080}" -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 run_app:app
 
 
